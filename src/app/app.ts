6145a421a449d80a34d081d45f25e1c46e928781
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-12-30 20:17:37 +0530

    Creating Components using CLI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './products/products';
+import { ProductComponent } from './product/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent],
+  imports: [RouterOutlet, ProductsComponent, ProductComponent], // to actually use the component in the app you have to import it here
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product';
 
 @Component({
   selector: 'products', // can be used in other files as <products></products>, also ".", "#", we can use these symbols to present it as a class or ID in div tag
   templateUrl: './products.html',
+  imports: [ProductComponent],
 })
 export class ProductsComponent {}
