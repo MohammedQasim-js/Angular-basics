55b50bff26b81d32e4e999f55f1ec57e19863f15
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-12-30 20:24:16 +0530

    Inline Template in Components

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'product',
-  templateUrl: './product.html',
+  template: '<p>Product 1</p>', // You can use `` for multiple lines of HTML code, and '' for single line of HTML code
   styleUrl: './product.css',
 })
 export class ProductComponent {}
