c9d2e3ddf2522f5fe7d1065c0d391c6f9c9042fc
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-12-30 18:18:44 +0530

    init and component creation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('myNewApp');
---
+++ src/app/products/products.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products', // can be used in other files as <products></products>, also ".", "#", we can use these symbols to present it as a class or ID in div tag
+  templateUrl: './products.html',
+})
+export class ProductsComponent {}
