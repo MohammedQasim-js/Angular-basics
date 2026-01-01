b6b474841a91a31f40f6e3e90650f5c83193d76d
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-01 17:29:54 +0530

    Way of selecting compnents, Selector

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'product',
+  selector: '[product]', // can be used as an attribute in other files like <div product></div>
   template: '<p>Product 1</p>', // You can use `` for multiple lines of HTML code, and '' for single line of HTML code
   styles: ['p {color: blue;}'],
 })
