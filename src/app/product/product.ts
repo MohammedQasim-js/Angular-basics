9e7d0cbd5d81a7f3dba580ac3a113c59afc91bba
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-01 17:22:38 +0530

    Different stylings

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'product',
   template: '<p>Product 1</p>', // You can use `` for multiple lines of HTML code, and '' for single line of HTML code
-  styleUrl: './product.css',
+  styles: ['p {color: blue;}'],
 })
 export class ProductComponent {}
