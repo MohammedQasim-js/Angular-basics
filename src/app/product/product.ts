b60ae888cf61241bb1017d476e17cae3bc2823dd
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-01 17:53:46 +0530

    Data binding - String Interpolation

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[product]', // can be used as an attribute in other files like <div product></div>
-  template: '<p>Product 1</p>', // You can use `` for multiple lines of HTML code, and '' for single line of HTML code
+  templateUrl: './product.html',
+  // template: '<p>Product 1</p>', // You can use `` for multiple lines of HTML code, and '' for single line of HTML code
   styles: ['p {color: blue;}'],
 })
-export class ProductComponent {}
+export class ProductComponent {
+  productId: number = 10;
+  stockStatus: string = 'Yes';
+  getStockStatus() {
+    return this.stockStatus;
+  }
+}
