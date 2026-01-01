e8981598d54245b5217b277364c5bd4027872ae8
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-01 19:58:14 +0530

    Data binidng - Two way Data binding

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from '../product/product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'products', // can be used in other files as <products></products>, also ".", "#", we can use these symbols to present it as a class or ID in div tag
   templateUrl: './products.html',
-  imports: [ProductComponent],
+  imports: [ProductComponent, FormsModule],
 })
 export class ProductsComponent {
   addProduct = true;
@@ -18,7 +20,7 @@ export class ProductsComponent {
   }
 
   onClick() {
-    this.addProductStatus = 'Product added!';
+    this.addProductStatus = 'Product added! and entered value is ' + this.enteredValue;
   }
 
   onUpdateProductName(event: any) {
